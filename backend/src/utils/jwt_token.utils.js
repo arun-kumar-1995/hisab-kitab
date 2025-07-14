c164c08216c3afbe0d960b85a89b600a3871239a
arun kumar <arun.devpro@gmail.com>
2025-07-14 12:15:08 +0530

    user logout

--- backend/src/utils/jwt_token.utils.js
+++ backend/src/utils/jwt_token.utils.js
@@ -1,8 +1,6 @@
 import Jwt from "jsonwebtoken";
 const TOKEN_SECRET = process.env.TOKEN_SECRET;
 
-
 export const GenerateToken = async (payload) => {
-  console.log(payload);
   return await Jwt.sign(payload, TOKEN_SECRET, { expiresIn: "7d" });
 };
